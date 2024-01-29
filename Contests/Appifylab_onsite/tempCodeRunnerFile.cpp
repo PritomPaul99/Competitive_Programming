242267b3c8fd07bb0c1f2559662ad6ebef90fe26
Pritom Paul <pritompaul1920@gmail.com>
2024-01-30 00:01:49 +0600

    First commit after new setup

---
+++ Contests/Appifylab_onsite/tempCodeRunnerFile.cpp
@@ -0,0 +1,19 @@
+else if (op == 3)
+        // {
+        //     int k;
+        //     cin >> k;
+
+        //     cout << str[k - 1] << nl;
+        // }
+        // else if (op == 4)
+        // {
+        //     if (f == 1)
+        //     {
+        //         str = bu;
+        //     }
+        //     else if (f == 2)
+        //     {
+        //         str = bu;
+        //     }
+        // }
+        // f = op;
