9c7c6b537b89fc61807e2ab5e3029d7e6fe3d21b
Pritom Paul <pritompaul1920@gmail.com>
2024-05-09 20:22:40 +0600

    new file 'Practice_Coding_Java' added for java codes for cpp

---
+++ Practice_Coding_Cpp/test.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    int x = 21, i = 18;
+    cout << __gcd(x, i) + i << endl;
+    return false;
+}
---
+++ Practice_Coding_Java/CodeForces_Solutions/src/A_Maximum.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class A_Maximum {
+    public static void main(String[] args) {
+        Scanner newObj = new Scanner(System.in);
+        int n;
+        
+    }
+}
